de3cd526cbeac627090f088d0c3bef7bde0dbeab
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-09-18 13:38:56 +0600

    added all types of dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './styles/style.scss';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
