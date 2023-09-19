dc54326e14bb2f5b2f8fe0fceee20ad2d1b0639f
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-09-19 17:01:07 +0800

    added footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { BiLogoFacebook, BiLogoLinkedin } from 'react-icons/bi';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer>
+        <div className="container-fluid pt-5 pb-3">
+          <div className="row">
+            <div className="col-4">
+              <p className="mb-0 text-white">
+                &copy; <strong>uaecopier</strong> {new Date().getFullYear()} All
+                write reserved
+              </p>
+            </div>
+            <div className="col-4 ">
+              <div className="text-center">
+                <Link className="me-3">Support</Link>
+                <Link className="me-3">Contact</Link>
+                <Link className="me-3">Disclaimer</Link>
+                <Link>Terms</Link>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="text-end">
+                <Link className="me-3">
+                  <AiOutlineTwitter />
+                </Link>
+                <Link className="me-3">
+                  <BiLogoFacebook />
+                </Link>
+                <Link className="me-3">
+                  <BiLogoLinkedin />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
