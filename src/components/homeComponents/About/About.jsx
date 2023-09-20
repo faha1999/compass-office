5c5b3246774d365a40fff07f09d62008df6cdfd7
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-09-20 22:56:59 +0800

    complete about

---
+++ src/components/homeComponents/About/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const About = () => {
+  return (
+    <>
+      <div className="about pt-4 pb-2">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4 text-center">
+              <h2 className="pb-2">Ghobash Group</h2>
+              <p className="pb-2">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse,
+                voluptatibus necessitatibus. Magni, impedit ipsam quo porro
+                eveniet neque quasi sint obcaecati rem illo suscipit unde hic
+                sit fuga possimus quisquam distinctio labore consequuntur
+                temporibus sapiente iusto? Accusamus harum optio beatae,
+                explicabo ad temporibus unde animi ea voluptatem exercitationem
+                quaerat illo?
+              </p>
+
+              <Link className="learnMore">Learn More</Link>
+            </div>
+            <div
+              className="col-4"
+              style={{
+                'background-image':
+                  'url(https://images.unsplash.com/photo-1531545514256-b1400bc00f31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWJvdXQlMjB1c3xlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60)',
+                borderRadius: '1rem',
+              }}
+            >
+              <div className="about-text text-center">
+                <h1>About Us</h1>
+              </div>
+            </div>
+            <div className="col-4 text-center">
+              <h2 className="pb-2">Ghobash Group</h2>
+              <p className="pb-2">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse,
+                voluptatibus necessitatibus. Magni, impedit ipsam quo porro
+                eveniet neque quasi sint obcaecati rem illo suscipit unde hic
+                sit fuga possimus quisquam distinctio labore consequuntur
+                temporibus sapiente iusto? Accusamus harum optio beatae,
+                explicabo ad temporibus unde animi ea voluptatem exercitationem
+                quaerat illo?
+              </p>
+
+              <Link className="learnMore">Learn More</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hero } from '../components/homeComponents/hero/Hero';
+import { About } from '../components/homeComponents/About/About';
 
 export const Home = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
