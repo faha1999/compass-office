0bb3f5f8ec7f4c6e70c53aee4b4d967879db300c
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-10-05 17:46:55 +0800

    complete FooterTop

--- src/layout/footer/dist/FooterTop.jsx
+++ src/layout/footer/dist/FooterTop.jsx
@@ -11,48 +11,49 @@ export const FooterTop = () => {
   return (
     <>
       <div className="row FooterTop pt-5 pb-5">
-        <div className="col-6 gap-3">
+        <div className="pb-4 col-xs-12 col-sm-12 col-md-6 gap-3">
           <h1 className="text-center pb-3">CONTACT US</h1>
-
-          <div className="d-flex justify-content-between">
-            <div className="col-6 d-flex contactInfo">
-              <MdLocationPin />
-
-              <p>
-                Address: UAE Copier 36, 7 states buiding, Deira , Dubai, UAE,
-                <br />
-                P.O box: 380750
-              </p>
-            </div>
-
-            <div className="col-6 contactInfo">
-              <div className="d-flex">
-                <BsFillTelephoneFill />
+          <div className="row">
+            <div className="flex-sm-column d-md-flex flex-md-row justify-content-between">
+              <div className="col-sm-12 col-md-6 d-flex contactInfo">
+                <MdLocationPin />
 
                 <p>
-                  Phone: <a href="tel:+971501726313">+971 50 172 6313</a>
+                  Address: UAE Copier 36, 7 states buiding, Deira , Dubai, UAE,
+                  <br />
+                  P.O box: 380750
                 </p>
               </div>
 
-              <div className="d-flex">
-                <FaFax />
+              <div className="col-sm-12 col-md-6 contactInfo">
+                <div className="d-flex">
+                  <BsFillTelephoneFill />
 
-                <p>Fax: +000 1 2345 6789</p>
-              </div>
+                  <p>
+                    Phone: <a href="tel:+971501726313">+971 50 172 6313</a>
+                  </p>
+                </div>
 
-              <div className="d-flex">
-                <MdEmail />
+                <div className="d-flex">
+                  <FaFax />
 
-                <p>
-                  Email:{' '}
-                  <a href="mailto:info@uaecopier.ae">info@uaecopier.ae</a>
-                </p>
+                  <p>Fax: +000 1 2345 6789</p>
+                </div>
+
+                <div className="d-flex">
+                  <MdEmail />
+
+                  <p>
+                    Email:{' '}
+                    <a href="mailto:info@uaecopier.ae">info@uaecopier.ae</a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="col-6">
+        <div className="pb-4 col-xs-12 col-sm-12 col-md-6">
           <h1 className="text-center pb-3">FOLLOW US</h1>
 
           <div className="d-flex justify-content-center gap-5 socialIcons">
