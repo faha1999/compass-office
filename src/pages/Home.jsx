240dc041c95299ae1aeef3d53bce5d6c95985522
Kawsar Ahmed Fahad <54038576+faha1999@users.noreply.github.com>
2023-10-21 00:50:35 +0800

    Subscription removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { About } from '../components/homeComponents/About/About';
 import { Wrapper } from '../components/homeComponents/wrapper/Wrapper';
 import { WhyUs } from '../components/homeComponents/whyUs/WhyUs';
 import { WhatWeOffer } from '../components/homeComponents/whatWeOffer/WhatWeOffer';
-import { Subscription } from '../components/homeComponents/subscription/Subscription';
-import { LatestNews } from '../components/homeComponents/latestNews/LatestNews';
+// import { Subscription } from '../components/homeComponents/subscription/Subscription';
+// import { LatestNews } from '../components/homeComponents/latestNews/LatestNews';
 
 export const Home = () => {
   return (
@@ -15,7 +15,7 @@ export const Home = () => {
       <Wrapper />
       <WhyUs />
       <WhatWeOffer />
-      <Subscription />
+      {/* <Subscription /> */}
       {/* <LatestNews /> */}
     </>
   );
